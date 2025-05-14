030b00c1e36a8e4a409b590abd76959d38c2b29a
Farng Hwai Yew <72641733+farnghwai@users.noreply.github.com>
2025-05-14 16:15:03 +0800

    add prerender setup for manifest.json route.

--- src/routes/manifest.json/+server.ts
+++ src/routes/manifest.json/+server.ts
@@ -1,6 +1,8 @@
 import { json } from '@sveltejs/kit';
 import { base } from '$app/paths';
 
+export const prerender = true;
+
 export const GET = () => {
 	return json({
 		name: 'Spider Solitaire',
